d03a9432f104191f116079db8530619121717d92
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-06 21:09:32 -0800

    created api route for reviews for a listing

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -4,7 +4,16 @@ from app.models import Review
 review_routes = Blueprint('reviews', __name__)
 
 
-@review_routes('/')
-def reviews():
-    reviews = Review.query.all()
+# @review_routes.route('/')
+# def reviews():
+#     reviews = Review.query.all()
+#     return {'reviews': [review.to_dict() for review in reviews]}
+
+@review_routes.route('/<id>')
+def review(id):
+    """
+    Return all reviews for a listing.
+    """
+
+    reviews = Review.query.filter_by(product_id=id).all()
     return {'reviews': [review.to_dict() for review in reviews]}
