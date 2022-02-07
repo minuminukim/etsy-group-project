f823b5938f61fe063116ff44f6eb722fb66a046f
Minu Kim <minumoseskim@gmail.com>
2022-02-07 13:17:51 -0500

    Set up product reducer

---
+++ react-app/src/store/productReducer.js
@@ -0,0 +1,35 @@
+const ADD_PRODUCT = 'product/addProduct';
+
+const addProduct = (product) => ({
+  type: ADD_PRODUCT,
+  product,
+});
+
+export const postProduct = (product) => async (dispatch) => {
+  const { userId, title, category, description, price, stock } = product;
+
+  const response = await fetch('/api/products', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: { user_id: userId, title, category, description, price, stock },
+  });
+
+  const data = await response.json();
+  dispatch(addProduct(data.newProduct));
+
+  return response;
+};
+
+const productReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        [action.product.id]: action.product,
+      };
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
