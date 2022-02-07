f6c239beed23dab51cae0f289755652b5d94f61d
Minu Kim <minumoseskim@gmail.com>
2022-02-07 00:41:50 -0500

    Build product listing form class

--- app/api/product_listing_routes.py
+++ app/api/product_listing_routes.py
@@ -14,3 +14,20 @@ def get_products():
     # print('product@@@@@@@@@@@@@@@@@@', products[1])
     # return "Hello World"
     return {"products": [product.to_dict() for product in products]}
+
+
+@product_listing_routes.route("/<int:id>")
+def get_product_by_id(id):
+    product = Product.query.get(id)
+    return product.to_dict()
+
+
+@product_listing_routes.route("/<int:id>/reviews")
+def get_product_reviews(id):
+    pass
+
+
+@product_listing_routes.route("/", methods=["POST"])
+def add_product():
+    pass
+
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,3 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .product_listing_form import ProductListingForm
