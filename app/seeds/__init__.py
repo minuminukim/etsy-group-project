9a0a6c07e7cd0a7003ef1da50a274200c99260ba
Minu Kim <minumoseskim@gmail.com>
2022-02-06 21:25:28 -0500

    Add product_images seed data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .products import seed_products, undo_products
+from .product_images import seed_product_images, undo_product_images
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup("seed")
 def seed():
     seed_users()
     seed_products()
+    seed_product_images()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_products()
+    undo_product_images()
     # Add other undo functions here
