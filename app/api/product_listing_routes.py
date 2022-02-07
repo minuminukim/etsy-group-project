35cdb8d78da65d2330f94697fad2764a53df8366
Minu Kim <minumoseskim@gmail.com>
2022-02-06 22:36:03 -0500

    Set up and register /api/products blueprint and test hello world

---
+++ app/api/product_listing_routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from app.models import Product, ProductImage, db
+from flask_login import login_required
+
+product_listing_routes = Blueprint("product_listing", __name__)
+
+
+@product_listing_routes.route("/")
+def get_products():
+    """
+    Returns all product listings.
+    """
+    products = Product.query.all()
+    print(products)
+    return "Hello World"
+    # return {"products": [product.to_dict() for product in products]}
