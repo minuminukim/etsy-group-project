80914cb846a4531e7d71df793d0ead0fca53cbd8
Minu Kim <minumoseskim@gmail.com>
2022-02-07 19:26:51 -0500

    Start adding basic styles to product listing form

--- react-app/src/components/ProductListingForm/ProductListingForm.js
+++ react-app/src/components/ProductListingForm/ProductListingForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import InputField from '../common/InputField';
 import { postProduct } from '../../store/productReducer';
+import './ProductListingForm.css';
 
 const ProductListingForm = ({ sessionUser }) => {
   const [title, setTitle] = useState('');
@@ -42,58 +43,85 @@ const ProductListingForm = ({ sessionUser }) => {
   const updateStock = (e) => setStock(e.target.value);
 
   return (
-    <form onSubmit={handleSubmit} className="product-listing-form">
-      <h2>Listing details</h2>
-      <InputField
-        label="Title"
-        id="title"
-        size="medium"
-        value={title}
-        onChange={updateTitle}
-        error={errors.title}
-      />
-      <label>
-        Category
-        <select
-          value={category}
-          onChange={updateCategory}
-          error={errors.category}
-        >
-          <option value="keyboards">Keyboards</option>
-          <option value="switches">Switches</option>
-          <option value="keycaps">Keycaps</option>
-          <option value="diy_kits">DIY Kits</option>
-        </select>
-      </label>
-      <InputField
-        label="Description"
-        id="description"
-        size="medium"
-        value={description}
-        onChange={updateDescription}
-        error={errors.description}
-      />
-      <h2>Inventory and pricing</h2>
-      <InputField
-        label="Price"
-        id="price"
-        size="medium"
-        type="number"
-        value={price}
-        onChange={updatePrice}
-        error={errors.price}
-      />
-      <InputField
-        label="Stock"
-        id="stock"
-        size="medium"
-        type="number"
-        value={stock}
-        onChange={updateStock}
-        error={errors.stock}
-      />
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="container">
+      <h1 className="page-heading">Add a new listing</h1>
+      <form onSubmit={handleSubmit} className="product-listing-form">
+        <h2 className="form-heading">Listing details</h2>
+        <h3 className="form-caption">
+          Tell the world all about your item and why they'll love it.
+        </h3>
+        <InputField
+          label="Title"
+          id="title"
+          size="medium"
+          value={title}
+          onChange={updateTitle}
+          error={errors.title}
+        />
+        <p className="input-field-details">
+          Include keywords that buyers would use to search for your item.
+        </p>
+        <label>
+          Category
+          <p className="input-field-details">
+            Choose a category that will help more shoppers find your item.
+          </p>
+          <select
+            value={category}
+            onChange={updateCategory}
+            error={errors.category}
+          >
+            <option value="keyboards">Keyboards</option>
+            <option value="switches">Switches</option>
+            <option value="keycaps">Keycaps</option>
+            <option value="diy_kits">DIY Kits</option>
+          </select>
+        </label>
+        <InputField
+          label="Description"
+          id="description"
+          size="medium"
+          value={description}
+          onChange={updateDescription}
+          error={errors.description}
+        />
+        <p className="input-field-details">
+          Start with a brief overview that describes your item's finest
+          features. Shoppers will only see the first few lines of your
+          description at first, so make it count!
+          <br /> Not sure what else to say? Shoppers also like hearing about
+          your process, and the story behind this item.
+        </p>
+        <h2 className="form-heading">Inventory and pricing</h2>
+        <InputField
+          label="Price"
+          id="price"
+          size="medium"
+          type="number"
+          value={price}
+          onChange={updatePrice}
+          error={errors.price}
+        />
+        <p className="input-field-details">
+          Remember to factor in the costs of materials, labor, and other
+          business expenses.
+        </p>
+        <InputField
+          label="Stock"
+          id="stock"
+          size="medium"
+          type="number"
+          value={stock}
+          onChange={updateStock}
+          error={errors.stock}
+        />
+        <p className="input-field-details">
+          For quantities greater than one, this listing will renew automatically
+          until it sells out.
+        </p>
+        <input type="submit" value="Save and continue" />
+      </form>
+    </div>
   );
 };
 
