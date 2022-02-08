dbabe733588863ffac1d36907e6890aefda46c1f
grantrussell <grant.ellis.russell@gmail.com>
2022-02-08 08:13:11 -0600

    cleaned up search feature, 90% working

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -6,5 +6,7 @@ search_routes = Blueprint("search", __name__)
 
 @search_routes.route('/<string:query>')
 def search(query):
-       products = Product.query.filter(Product.title.contains(query))
+       products = Product.query.filter(Product.title.contains(query)).limit(16)
        return {"products": [product.to_dict() for product in products]}
+
+       ## the search is currently case sensitive
