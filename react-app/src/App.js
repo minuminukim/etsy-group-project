db2adf3ed89b09b3de14bd7a57a1306ecc4df473
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-07 23:39:46 -0800

    added reset btn to review form and simple css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,7 @@ import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
-import CreateReview from './components/Review';
+import CreateReview from './components/ReviewForm/Review';
 import { authenticate } from './store/session';
 
 function App() {
