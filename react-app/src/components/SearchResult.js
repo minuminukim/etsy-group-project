987ded07f4dee86e1cf53a505baa28ed228f8569
grantrussell <grant.ellis.russell@gmail.com>
2022-02-08 15:25:21 -0600

    added 404 Not Found on search page

--- react-app/src/components/SearchResult.js
+++ react-app/src/components/SearchResult.js
@@ -25,10 +25,13 @@ const SearchResult = () => {
         );
       });
 
+    const productNotFound = <div><h2>We couldn't find any results for {query}</h2><p>Try searching for something else instead?</p></div>
+
+
     return (
         <div>
-            <div>{productComponents}</div>
-       </div>
+            {(products.length>=1) ? productComponents : productNotFound}
+        </div>
     )
 }
 
