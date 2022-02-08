6ae5b3d78488a63705ed71b9ca65206cd4829dca
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-08 16:39:25 -0500

    Got image upload and preview working on frontend, currently testing multiple

--- react-app/src/components/ProductImageForm/ProductImageForm.js
+++ react-app/src/components/ProductImageForm/ProductImageForm.js
@@ -1,16 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Button from '../common/Button';
 
 const ProductImageForm = ({ sessionUser }) => {
   const [image, setImage] = useState(null);
+  const [images, setImages] = useState([]);
+  const [previews, setPreviews] = useState([]);
   const [count, setCount] = useState(1);
   const [errors, setErrors] = useState({});
   const [imageLoading, setImageLoading] = useState(false);
   const history = useHistory();
   const { productId } = useParams();
   const product = useSelector((state) => state.products[productId]);
+
+  useEffect(() => {
+    if (images && images[0]) {
+      setPreviews([]);
+      console.log('previews', previews);
+      console.log('images', images);
+      const objectURLs = images.reduce((acc, img) => {
+        return [...acc, URL.createObjectURL(img)];
+      }, []);
+      console.log('objectURLs', objectURLs);
+      setPreviews([...previews, ...objectURLs]);
+    }
+  }, [images]);
 
   const generateImageURL = (title) => {
     const split = title.trim().toLowerCase().split(' ');
@@ -34,27 +49,101 @@ const ProductImageForm = ({ sessionUser }) => {
 
     if (res.ok) {
       await res.json();
-      console.log('oik@@@@@@@@@@@@@@@@@@@@@@@@');
       setImageLoading(false);
       history.push('/');
     } else {
       setImageLoading(false);
       const data = await res.json();
-      console.log('@@@@@@@@@@@@@@@@@', data);
+
       console.log('error');
     }
   };
 
-  const updateImage = (e) => {
-    const file = e.target.files[0];
-    setImage(file);
+  // const updateImage = (e) => {
+  //   const file = e.target.files[0];
+  //   console.log('@@@@@@@@@@@@@@@@', e.target.files);
+  //   setImage(file);
+  // };
+
+  const updateImages = (e) => {
+    setImages([]);
+    const files = [...e.target.files];
+    console.log('files', files);
+    const userImages = files.reduce((acc, f) => [...acc, f], []);
+    console.log('userImages', userImages);
+    setImages([...userImages]);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" accept=".png,.jpg,.jpeg,.gif" onChange={updateImage} />
-      <Button label="Submit" className="submit-button" type="submit" />
-    </form>
+    <div className="container">
+      <h1 className="page-heading">Add a new listing</h1>
+      <form onSubmit={handleSubmit} className="product-image-form">
+        <h2 className="form-heading">Photos</h2>
+        <div className="form-row">
+          <div className="input-field-details">
+            <p className="input-field-details">
+              Use up to five photos to show your item's most important
+              qualities.
+              <br /> Tips:
+            </p>
+            <ul>
+              <li>Use natural light and no flash.</li>
+              <li>Include a common object for scale.</li>
+              <li>Show the item being held or used.</li>
+              <li>Shoot against a clean, simple background.</li>
+              <li>
+                Add photos to your variations so buyers can see all their
+                options.
+              </li>
+            </ul>
+          </div>
+          <input
+            type="file"
+            accept=".png,.jpg,.jpeg,.gif"
+            onChange={updateImages}
+            multiple
+          />
+        </div>
+        <div className="image-preview-grid">
+          <div className="grid-block grid-block-0">
+            <img
+              src={previews[0] || null}
+              className="preview-image"
+              alt="Preview image"
+            />
+          </div>
+          <div className="grid-block grid-block-1">
+            <img
+              src={previews[1] || null}
+              className="preview-image"
+              alt="Preview image"
+            />
+          </div>
+          <div className="grid-block grid-block-2">
+            <img
+              src={previews[2] || null}
+              className="preview-image"
+              alt="Preview image"
+            />
+          </div>
+          <div className="grid-block grid-block-3">
+            <img
+              src={previews[3] || null}
+              className="preview-image"
+              alt="Preview image"
+            />
+          </div>
+          <div className="grid-block grid-block-4">
+            <img
+              src={previews[4] || null}
+              className="preview-image"
+              alt="Preview image"
+            />
+          </div>
+        </div>
+        <Button label="Submit" className="submit-button" type="submit" />
+      </form>
+    </div>
   );
 };
 
