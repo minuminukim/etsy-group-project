7b5296fb81f6d9e58fc6582b751ba2d1a8374f70
grantrussell <grant.ellis.russell@gmail.com>
2022-02-08 09:04:07 -0600

    fixed product navlinks to redirect to product listings in search result

--- react-app/src/components/SearchResult.js
+++ react-app/src/components/SearchResult.js
@@ -10,7 +10,6 @@ const SearchResult = () => {
     useEffect(() => {
     async function fetchData() {
       const query = new URLSearchParams(search).get('q')
-      console.log(query)
       const response = await fetch(`/api/search/${query}`);
       const responseData = await response.json();
       setProducts(responseData.products);
@@ -18,19 +17,10 @@ const SearchResult = () => {
       fetchData();
     }, [query]);
 
-    // const productComponents = products.map((product) => {
-    //     if (product.title.toLowerCase().includes(query.toLowerCase())) {
-    //         return (
-    //             <li key={product.id}>
-    //                 <NavLink to={`/products/${product.id}`}>{product.title}</NavLink>
-    //             </li>
-    //         )};
-    // });
-
     const productComponents = products.map((product) => {
         return (
           <li key={product.id}>
-            <NavLink to={`/users/${product.id}`}>{product.title}</NavLink>
+            <NavLink to={`/products/${product.id}`}>{product.title}</NavLink>
           </li>
         );
       });
