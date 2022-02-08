084cd325c3fd0ae6d3a26716d336358f49e8feed
grantrussell <grant.ellis.russell@gmail.com>
2022-02-08 12:36:33 -0600

    added linked to categories on landing page

--- app/api/category_routes.py
+++ app/api/category_routes.py
@@ -5,6 +5,10 @@ category_routes = Blueprint("category", __name__)
 
 @category_routes.route('/<string:category>')
 def category_search(category):
-      print(category)
       products = Product.query.filter(Product.category == category)
       return {"products": [product.to_dict() for product in products]}
+
+@category_routes.route('/sale')
+def on_sale():
+    products = Product.query.filter(Product.discount > 0)
+    return {"products": [product.to_dict() for product in products]}
--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -3,11 +3,21 @@ import { NavLink } from "react-router-dom";
 const LandingPage = () => {
 
     const categoryA = "switches"
+    const categoryB = "keycaps"
+    const categoryC = "keyboards"
+    const categoryD = "diy_kits"
+    const categoryE = "sale"
 
     return (
         <div>
             <h1>Landing Page</h1>
-            <NavLink to={`/category/${categoryA}`}>{categoryA}</NavLink>
+            <ul>
+                <li><NavLink to={`/category/${categoryA}`}>{categoryA}</NavLink></li>
+                <li><NavLink to={`/category/${categoryB}`}>{categoryB}</NavLink></li>
+                <li><NavLink to={`/category/${categoryC}`}>{categoryC}</NavLink></li>
+                <li><NavLink to={`/category/${categoryD}`}>{categoryD}</NavLink></li>
+                <li><NavLink to={`/category/sale`}>{categoryE}</NavLink></li>
+            </ul> 
         </div>
     )
 }
