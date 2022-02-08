06b2657404103b251b64206454d8aee5367b9481
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-07 20:10:57 -0500

    Add review reducer import statement

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
 import productReducer from './productReducer';
+import review from './review';
 
 const rootReducer = combineReducers({
   session,
