b3ba62eea343276c2e13288be0a1b538eed282f0
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-10 11:02:04 -0800

    review components mostly complete, still needs to remove ReviewForm component when a user review already exists for product

--- react-app/src/components/ReviewForm/ReviewForm.js
+++ react-app/src/components/ReviewForm/ReviewForm.js
@@ -1,137 +1,143 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux"
-import { useParams, useHistory } from "react-router-dom";
 import * as sessionActions from "../../store/review"
+import { useParams } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs"
 import "./ReviewForm.css"
 
-import { BsStarFill } from "react-icons/bs"
-
 const CreateReview = () => {
-    const history = useHistory()
-    const dispatch = useDispatch()
-    const currentUser = useSelector(state => state.session.user);
-    const [rating, setRating] = useState(0)
-    const [body, setBody] = useState("")
-    const [displayBtn, setDisplayBtn] = useState(false)
-    const [errors, setErrors] = useState([])
-    const [test, setTest] = useState(false)
-    const reviews = useSelector(state => state.review.reviews);
-    let { productId } = useParams()
+  const dispatch = useDispatch()
+  const currentUser = useSelector(state => state.session.user);
+  const reviews = useSelector(state => state.review.reviews);
+  const [rating, setRating] = useState(0)
+  const [body, setBody] = useState("")
+  const [displayBtn, setDisplayBtn] = useState(false)
+  const [errors, setErrors] = useState([])
+  const [test, setTest] = useState(false)
+  const [displayReviewForm, setDisplayReviewForm] = useState(true)
+  let { productId } = useParams()
 
 
+  // TODO - fix review form display on refresh when review for use already exists
+  // const reviewExists = () => {
+  //   reviews?.reviews?.some(review => {
+  //     return (review?.user_id === currentUser.id)
+  //   })
+  // }
 
+  console.log(reviews?.reviews)
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        // setTest(!test)
+  // reviewExists() === true ? setDisplayReviewForm(true) : setDisplayReviewForm(false)
 
-        const payload = {
-            user_id: currentUser.id,
-            product_id: productId,
-            rating,
-            body,
+  console.log(displayReviewForm)
 
-        }
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        dispatch(sessionActions.newReview(payload)).catch(async (res) => {
-            // dispatch(sessionActions.getReviews(productId))
-
-
-            console.log(res)
-            const data = await res.json();
-            console.log('THIS IS DATA ----', data)
-            if (data.errors) {
-                setErrors(data.errors);
-            }
-        })
-        setTest(!test)
+    const payload = {
+      user_id: currentUser.id,
+      product_id: productId,
+      rating,
+      body,
 
     }
 
-    let btn;
-    if (displayBtn) {
-        btn = (
-            <div id="review_btn_container">
-                <button className="review-btn" className="btn"
-                    onClick={() => {
-                        setErrors([])
-                        setBody("")
-                        setDisplayBtn(false)
-                    }}>Reset</button>
-                <button className="review-btn" className="btn">Submit</button>
-            </div>
+    dispatch(sessionActions.newReview(payload)).catch(async (res) => {
 
-        )
-    } else {
-        btn = (
-            null
-        )
-    }
+      const data = await res.json();
+      if (data.errors) {
+        setErrors(data.errors);
+      }
+    })
+    setDisplayReviewForm(false)
+    setTest(!test)
+  }
 
-    useEffect(() => {
-        console.log('inside useeffect')
-        dispatch(sessionActions.getReviews(productId))
-    }, [dispatch, test])
+  let btn;
+  if (displayBtn) {
+    btn = (
+      <div id="review_btn_container">
+        <button className="review-btn" className="btn"
+          onClick={() => {
+            setErrors([])
+            setBody("")
+            setDisplayBtn(false)
+          }}>Cancel</button>
+        <button className="review-btn" className="btn">Submit</button>
+      </div>
 
-    useEffect(() => {
-        // dispatch(sessionActions.newReview(productId))
-        dispatch(sessionActions.getReviews(productId))
-    }, [dispatch, test])
-
-
-    return (
-        <form id="review_form" onSubmit={onSubmit} value={true}
-            onFocus={(e) => {
-                setDisplayBtn(true)
-            }}>
-            <div>
-                {errors.body}
-                <div>
-
-                    {errors.rating}
-                </div>
-            </div>
-            {/* <label>Rating</label> */}
-            <div>
-                <BsStarFill
-                    onClick={(e) => {
-                        setRating(1);
-                    }} />
-                <BsStarFill
-                    onClick={(e) => {
-                        setRating(4);
-                    }} />
-                <BsStarFill
-                    onClick={(e) => {
-                        setRating(6);
-                    }} />
-                <BsStarFill
-                    onClick={(e) => {
-                        setRating(8);
-                    }} />
-                <BsStarFill
-                    onClick={(e) => {
-                        setRating(10);
-                    }} />
-            </div>
-
-            <div id='review-pic'>
-                <img className="profile-pic" src={currentUser.profile_pic_url} />
-                <textarea
-                    id="review_body"
-                    placeholder="Add a public review..."
-                    maxLength="1000"
-                    value={body}
-                    onChange={(e) => {
-                        setBody(e.target.value)
-                    }}
-                >
-                </textarea>
-            </div>
-
-            {btn}
-        </form>
     )
+  } else {
+    btn = (
+      null
+    )
+  }
+
+  useEffect(() => {
+    dispatch(sessionActions.getReviews(productId))
+  }, [dispatch, test])
+
+  useEffect(() => {
+    dispatch(sessionActions.getReviews(productId))
+  }, [dispatch, test])
+
+
+  return (
+    <>
+      {displayReviewForm ?
+        <form id="review_form" onSubmit={onSubmit} value={true}
+          onFocus={(e) => {
+            setDisplayBtn(true)
+          }}>
+          <div>
+            {errors.body}
+            <div>
+
+              {errors.rating}
+            </div>
+          </div>
+          <div>
+            <BsStarFill
+              onClick={(e) => {
+                setRating(1);
+              }} />
+            <BsStarFill
+              onClick={(e) => {
+                setRating(4);
+              }} />
+            <BsStarFill
+              onClick={(e) => {
+                setRating(6);
+              }} />
+            <BsStarFill
+              onClick={(e) => {
+                setRating(8);
+              }} />
+            <BsStarFill
+              onClick={(e) => {
+                setRating(10);
+              }} />
+          </div>
+
+          <div id='review-pic'>
+            <img className="profile-pic" src={currentUser?.profile_pic_url} alt="profile-pic" />
+            <textarea
+              id="review_body"
+              placeholder="Add a public review..."
+              maxLength="1000"
+              value={body}
+              onChange={(e) => {
+                setBody(e.target.value)
+              }}
+            >
+            </textarea>
+          </div>
+
+          {btn}
+        </form>
+        : null}
+    </>
+  )
 }
 
 
