6f928a13db9de0216920cc3147f8aa94676e7130
grantrussell <grant.ellis.russell@gmail.com>
2022-02-09 18:05:35 -0600

    added styling to top of landing page, with category icons

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import "./LandingPage.css";
 
 const LandingPage = () => {
 
@@ -10,14 +11,35 @@ const LandingPage = () => {
 
     return (
         <div>
-            <h1>Landing Page</h1>
-            <ul>
-                <li><NavLink to={`/category/${categoryA}`}>{categoryA}</NavLink></li>
-                <li><NavLink to={`/category/${categoryB}`}>{categoryB}</NavLink></li>
-                <li><NavLink to={`/category/${categoryC}`}>{categoryC}</NavLink></li>
-                <li><NavLink to={`/category/${categoryD}`}>{categoryD}</NavLink></li>
-                <li><NavLink to={`/category/sale`}>{categoryE}</NavLink></li>
-            </ul> 
+            <div className="category-container">
+                <div className="welcome-container">
+                    <p>Find extra-special ways to express your keyboard.</p>
+                </div>
+                <div className="category-list">
+                    <ul className="category-list">
+                        <div className="category-list-item">
+                            <img className="category-image" src="https://qwerty-project-bucket.s3.amazonaws.com/product_images/cherry-red-switches-1.jpeg"/>
+                            <li><NavLink to={`/category/${categoryA}`}>Switches</NavLink></li>
+                        </div>
+                        <div className="category-list-item">
+                            <img className="category-image" src="https://qwerty-project-bucket.s3.amazonaws.com/product_images/pbt-chalk-keycaps-1.jpeg"/>
+                            <li><NavLink to={`/category/${categoryB}`}>Keycaps</NavLink></li>
+                        </div>
+                        <div className="category-list-item">
+                            <img className="category-image" src="https://qwerty-project-bucket.s3.amazonaws.com/product_images/kbdpad-numpad-keyboard-4.jpeg"/>
+                            <li><NavLink to={`/category/${categoryC}`}>Keyboards</NavLink></li>
+                        </div>
+                        <div className="category-list-item">
+                            <img className="category-image" src="https://qwerty-project-bucket.s3.amazonaws.com/product_images/kbdpad-diy-kit-2.jpeg"/>
+                            <li><NavLink to={`/category/${categoryD}`}>DIY kits</NavLink></li>
+                        </div>
+                        <div className="category-list-item">
+                            <img className="category-image" src="https://qwerty-project-bucket.s3.amazonaws.com/product_images/cherry-red-switches-1.jpeg"/>
+                            <li><NavLink to={`/category/sale`}>Sale</NavLink></li>
+                        </div>
+                    </ul> 
+                </div>
+            </div>
         </div>
     )
 }
