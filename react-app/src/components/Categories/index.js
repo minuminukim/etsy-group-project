e8b527db6128b2773eccb00d0b1fed2ee707ad71
grantrussell <grant.ellis.russell@gmail.com>
2022-02-10 21:40:29 -0600

    added category view headers with some styling

--- react-app/src/components/Categories/index.js
+++ react-app/src/components/Categories/index.js
@@ -4,6 +4,8 @@ import ProductGridBlock from '../ProductCard';
 import { NavLink } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import "./Categories.css";
+import "../ButtonWithIcon/ButtonWithIcon.css";
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from 'react-icons/ai';
 
 const CategoryView = () => {
   const [products, setProducts] = useState([]);
@@ -28,18 +30,31 @@ const CategoryView = () => {
     );
   });
 
+  const CategoryTitle = category.split("_").join(" ")
+
+
+
   return (
     <div>
       <div className="category-header">
         <div className="header-elements">
+          {/* <div id="slideLeft" className="circle-btn circle-btn-medium"><AiOutlineArrowLeft/></div> */}
           <ul className="header-elements-container">
-            <li className="header-images"><img className="header-pic" src={products[0]?.images[0]}/></li>
-            <li className="header-images"><img className="header-pic" src={products[1]?.images[0]}/></li>
-            <li className="header-images"><img className="header-pic" src={products[2]?.images[0]}/></li>
-            <li className="header-images"><img className="header-pic" src={products[3]?.images[0]}/></li>
-            <li className="header-images"><img className="header-pic" src={products[4]?.images[0]}/></li>
+            <li className="header-images"><NavLink to={`/products/${products[0]?.id}`}><img className="header-pic" src={products[0]?.images[0]}/></NavLink></li>
+            <li className="header-images"><NavLink to={`/products/${products[1]?.id}`}><img className="header-pic" src={products[1]?.images[0]}/></NavLink></li>
+            <li className="header-images"><NavLink to={`/products/${products[2]?.id}`}><img className="header-pic" src={products[2]?.images[0]}/></NavLink></li>
+            <li className="header-images"><NavLink to={`/products/${products[3]?.id}`}><img className="header-pic" src={products[3]?.images[0]}/></NavLink></li>
+            <li className="header-images"><NavLink to={`/products/${products[4]?.id}`}><img className="header-pic" src={products[4]?.images[0]}/></NavLink></li>
           </ul>
+          {/* <div id="slideRight" className="circle-btn circle-btn-medium"><AiOutlineArrowRight/></div> */}
         </div>
+      </div>
+      <div className="category-content-header">
+          <div className="content-container">
+            <p className="by-interest">SHOP BY INTEREST</p>
+            <p className="category-capitalize">{CategoryTitle}</p>
+            <p className="category-explore">Explore extra-special ways to express your keyboard.</p>
+          </div>
       </div>
       <h2>Shop by interest</h2>
       <p>{category}</p>
