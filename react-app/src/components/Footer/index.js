5780cd895659c8c1054973663748b6eedffebf63
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-02-11 16:45:22 -0800

    add more styling to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "./Footer.css"
 import { BsGlobe } from "react-icons/bs"
+import logo from './footer-img.PNG'
+import logo2 from './footer-img2.PNG'
+import logo3 from './footer-img3.PNG'
 
 const Footer = () => {
   return (
     <footer>
+      <img src={logo3} id="row-img" />
       <div id='story-row'>
         <div id="story-row-inner">
           <h1 id="footer-title">What is Qwerty</h1>
         </div>
+        <div id="story-row-inner">
+          <div>
+            <p>A community doing good</p>
+          </div>
+          <div>
+            <p>Support independent creators</p>
+          </div>
+          <div>
+            <p>Peace of mind</p>
+          </div>
+        </div>
       </div>
+        <img src={logo2} id="row-img" />
+        <img src={logo} id="row-img" />
       <div id="row2">
         <BsGlobe size={23} />
         <p>
@@ -17,35 +34,34 @@ const Footer = () => {
         </p>
       </div>
       <div id="row3">
-        <table id="row3-inner">
+        <div id="row3-inner">
           <div>
-            <th className="row3-headers">Technologies</th>
-
-            <td>React</td>
-            <td>Redux</td>
-            <td>JavaScript</td>
-            <td>Python</td>
-            <td>SQLAlchemy</td>
-            <td>AWS S3</td>
-            <td>HTML</td>
-            <td>CSS</td>
+            <div className="row3-headers">Technologies</div>
+            <p>React</p>
+            <p>Redux</p>
+            <p>JavaScript</p>
+            <p>Python</p>
+            <p>SQLAlchemy</p>
+            <p>AWS S3</p>
+            <p>HTML</p>
+            <p>CSS</p>
           </div>
           <div>
-            <th className="row3-headers">Linkedin</th>
+            <p className="row3-headers">Linkedin</p>
             <a className='footer-link'  href='https://www.linkedin.com/in/steve-correa/' target="_blank">Steve Correa</a>
             <a className='footer-link'  href='https://www.linkedin.com/in/minu-kim-911bbb192/' target="_blank">Minu Kim</a>
             <a className='footer-link'  href='https://www.linkedin.com/in/grant-ellis-russell' target="_blank">Grant Russell</a>
             <a className='footer-link'  href='https://www.linkedin.com/in/eddie-verdusco/' target="_blank">Eddie Verdusco</a>
           </div>
           <div>
-            <th className="row3-headers">GitHub</th>
+            <p className="row3-headers">GitHub</p>
             <a className='footer-link'  href='https://github.com/scorrea310' target="_blank">Steve Correa</a>
             <a className='footer-link'  href='https://github.com/minuminukim' target="_blank">Minu Kim</a>
             <a className='footer-link'  href='https://github.com/grantongrant' target="_blank">Grant Russell</a>
             <a className='footer-link'  href='https://github.com/verduscos' target="_blank">Eddie Verdusco</a>
 
           </div>
-        </table>
+        </div>
       </div>
       <div id="row4">
         <div id="row4-inner">
