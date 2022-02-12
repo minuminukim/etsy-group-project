c6f4be4d7ba108a32c1785b41ff720a9d611cc8c
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-12 13:57:43 -0500

    Style masonry grid for landing page

--- react-app/src/components/ProductImage/ProductImage.js
+++ react-app/src/components/ProductImage/ProductImage.js
@@ -2,16 +2,14 @@ import { Link } from 'react-router-dom';
 import Badge from '../common/Badge';
 import './ProductImage.css';
 
-const ProductImage = ({ shape, src, alt, price, id }) => {
+const ProductImage = ({ shape, src, price, id, i }) => {
+  const style = { backgroundImage: `url(${src})` };
   return (
-    <Link to={`/products/${id}`}>
-      <div className={`product-image product-image-${shape}`}>
-        <img
-          src={src}
-          alt={alt}
-          className={`product-image product-image-${shape}`}
-        />
-        <Badge className="price-badge" text={`$${price}`} />
+    <Link to={`/products/${id}`} id={`product-image-${i}`}>
+      <div className="product-image-wrapper">
+        <div className={`product-image product-image-${shape}`} style={style}>
+          <Badge className="price-badge" text={`$${price}`} />
+        </div>
       </div>
     </Link>
   );
